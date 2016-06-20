ec5ed0353efa0c3a20376fb6756a8b5802c42140
Vincent Chau <vichau@ucdavis.edu>
2016-06-20 05:37:26 -0700

    first commit

---
+++ CodeSprint/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CodeSprint
+//
+//  Created by Vincent Chau on 6/20/16.
+//  Copyright © 2016 Vincent Chau. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
