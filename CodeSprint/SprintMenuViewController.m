6110da198e770161cea84f34330e98865eadb895
Vincent Chau <vichau@ucdavis.edu>
2016-07-27 23:08:03 -0700

    updated tableview

--- CodeSprint/SprintMenuViewController.m
+++ CodeSprint/SprintMenuViewController.m
@@ -66,6 +66,8 @@
 -(void)setupView{
     self.navigationItem.title = @"Teams";
     self.navigationItem.hidesBackButton = YES;
+    self.teamsTableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    
     UIBarButtonItem *newBackButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"back-icon"] style:UIBarButtonItemStylePlain target:self action:@selector(dismiss)];
     self.navigationItem.leftBarButtonItem = newBackButton;
     
@@ -103,10 +105,11 @@
     [[FirebaseManager sharedInstance].currentUser.groupsIDs addObject:inputText];
     [FirebaseManager createTeamWith:newTeam];
     NSLog(@"CREATE NEW TEAM CALLED");
-    // APPEND TEAM TO CREATOR (CURRENT USER)
+    [self.teamsTableView reloadData];
 }
 -(void)joinNewTeam:(NSString*)teamName{
     [FirebaseManager addUserToTeam:teamName andUser:[FirebaseManager sharedInstance].currentUser.uid];
+    [self.teamsTableView reloadData];
 }
 
 #pragma mark - UITableViewDelegate
@@ -114,7 +117,7 @@
 
     TeamsTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TeamCell" forIndexPath:indexPath];
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
-    cell.teamNameLabel.text = @"NEW LAbEL";
+    cell.teamNameLabel.text = [FirebaseManager sharedInstance].currentUser.groupsIDs[indexPath.row];
     return cell;
 }
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
